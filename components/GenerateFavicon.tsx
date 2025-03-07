6d91daf53a21c27846f753410a812044ea16ed38
AlAminKh03 <alaminkhan6203@gmail.com>
2025-03-08 01:01:02 +0600

    added seo header in about

--- components/GenerateFavicon.tsx
+++ components/GenerateFavicon.tsx
@@ -3,24 +3,20 @@
 export function GenerateFavicon() {
   return (
     <>
-      <link rel="icon" type="image/svg+xml" href="/furniture-favicon.svg" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <link
         rel="icon"
         type="image/png"
-        href="/furniture-favicon.png"
+        href="/favicon-32x32.png"
         sizes="32x32"
       />
       <link
         rel="icon"
         type="image/png"
-        href="/furniture-favicon-16x16.png"
+        href="/favicon-16x16.png"
         sizes="16x16"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/furniture-apple-touch-icon.png"
-      />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       <link rel="manifest" href="/site.webmanifest" />
     </>
   );
