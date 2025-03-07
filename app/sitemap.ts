e1ed9cfc257d7b237a2aa89efb6cf8eb21820353
AlAminKh03 <alaminkhan6203@gmail.com>
2025-03-08 01:26:02 +0600

    added seo header in about

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,16 +9,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://qatarfurnituredecor.com/products",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
-    },
-    {
       url: "https://qatarfurnituredecor.com/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8,
+      priority: 0.9,
+    },
+    {
+      url: "https://qatarfurnituredecor.com/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://qatarfurnituredecor.com/work",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: "https://qatarfurnituredecor.com/contact",
