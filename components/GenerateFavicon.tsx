db78adeca0f0f36fef197643428aa9f3e4025c96
AlAminKh03 <alaminkhan6203@gmail.com>
2025-03-08 12:56:27 +0600

    seo header added

--- components/GenerateFavicon.tsx
+++ components/GenerateFavicon.tsx
@@ -3,7 +3,14 @@
 export function GenerateFavicon() {
   return (
     <>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   );
 }
