c535ca6284a07b03d0a9d1f79d39615ffc61d3ee
AlAminKh03 <alaminkhan6203@gmail.com>
2025-03-11 01:25:46 +0600

    added seo header

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,21 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: [
+          "/",
+          "/about",
+          "/services",
+          "/work",
+          "/contact",
+          "/quote",
+          "/ar",
+        ],
+        disallow: ["/api/"],
+      },
+    ],
     sitemap: "https://qatarfurnituredecor.com/sitemap.xml",
   };
 }
